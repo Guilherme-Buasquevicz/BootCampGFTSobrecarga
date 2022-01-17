86ddf20ebde5883952e3b4bf0371d4cc007658c6
Guilherme-Buasquevicz <buasquevicz22@outlook.com>
2022-01-17 14:03:18 -0300

    Sobrecarga

---
+++ src/one/DigitalInnovation/BootCamp/Main.java
@@ -0,0 +1,21 @@
+package one.DigitalInnovation.BootCamp;
+
+public class Main {
+        public static void main(String[] args) {
+
+            //Geometria
+            Geometria.area(3);
+            Geometria.area(5d,5d);
+            Geometria.area(7,8,9);
+            Geometria.area(5f,5f);
+
+            System.out.println("---------------");
+            //Geometria
+            Geometria.area(7);
+            Geometria.area(7d,7d);
+            Geometria.area(7,8,9);
+            Geometria.area(5f,5f);
+        }
+}
+
+
